f7517a248ab08b82b8640cf6d61347edd02c4e0b
JD <jdakemp@gmail.com>
2023-04-07 12:49:09 -0400

    Created a main class and I am running class object in the main method. Need to still create a synchonus scheme in the main method.

---
+++ src/com/coderscampus/assignment/Main.java
@@ -0,0 +1,9 @@
+package com.coderscampus.assignment;
+
+public class Main {
+
+    public static void main(String arg[]) {
+        Assignment8 signmentObject = new Assignment8();
+        signmentObject.getNumbers();
+    }
+}
